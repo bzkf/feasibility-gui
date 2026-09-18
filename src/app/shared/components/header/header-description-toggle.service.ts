1a4f241edeae7b243dd97a76e7656bbb9c98cc7c
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2026-09-18 12:30:23 +0200

    hide header description for pages on start

--- src/app/shared/components/header/header-description-toggle.service.ts
+++ src/app/shared/components/header/header-description-toggle.service.ts
@@ -1,21 +1,21 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from '@angular/core'
+import { BehaviorSubject } from 'rxjs'
 
 @Injectable({ providedIn: 'root' })
 export class HeaderDescriptionToggleService {
-  private readonly STORAGE_KEY = 'header-description-visible';
+  private readonly STORAGE_KEY = 'header-description-visible'
 
-  private visible = new BehaviorSubject<boolean>(localStorage.getItem(this.STORAGE_KEY) !== 'false');
+  private visible = new BehaviorSubject<boolean>(localStorage.getItem(this.STORAGE_KEY) === 'true')
 
-  readonly visible$ = this.visible.asObservable();
+  readonly visible$ = this.visible.asObservable()
 
   get isVisible(): boolean {
-    return this.visible.value;
+    return this.visible.value
   }
 
   toggle() {
-    const next = !this.visible.value;
-    this.visible.next(next);
-    localStorage.setItem(this.STORAGE_KEY, String(next));
+    const next = !this.visible.value
+    this.visible.next(next)
+    localStorage.setItem(this.STORAGE_KEY, String(next))
   }
 }
